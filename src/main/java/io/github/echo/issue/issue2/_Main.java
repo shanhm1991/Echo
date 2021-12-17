feb928974ebb4ce842e4ea723e63551f1086f511
shanhm1991@163.com <shanhm1991@163.com>
2021-12-17 20:36:42 +0800

    命名修改test

---
+++ src/main/java/io/github/echo/issue/issue2/_Main.java
@@ -0,0 +1,20 @@
+package io.github.echo.issue.issue2;
+
+/**
+ * 
+ * @author shanhm1991
+ *
+ */
+public class _Main {
+
+	public static void main(String[] args) {
+		
+		Case1.test(); //14ms
+		System.out.println();
+		
+		Case2.test(); //12ms
+		System.out.println();
+		
+		Case3.test(); //8ms
+	}
+}
